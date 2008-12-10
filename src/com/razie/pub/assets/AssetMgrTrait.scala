6dd1e32c9f42be393fc4bfba8c5c4a99a902bdd6
razie <razie@razie>
2008-12-10 04:25:12 +0000

    long time no sync

--- src/com/razie/pub/assets/AssetMgrTrait.scala
+++ src/com/razie/pub/assets/AssetMgrTrait.scala
@@ -5,49 +5,50 @@ import com.razie.pub.assets._
 import scala.collection._
 import com.razie.pub.data._
 
-object FuckYou {
-   final val ORGANIZE = new ActionItem("organize")
-  }
 
 /** got only abstract methods so it turns into a java interface... */
 trait AssetMgrTrait {
   
-  type fun = (AssetKey, Referenceable, String, ScriptContext) => AnyRef
+    type fun = (AssetKey, Referenceable, String, ScriptContext) => AnyRef
 
-  private[this] implicit def str2ActionI (s:String) : ActionItem = new ActionItem (s)
+    private[this] implicit def str2ActionI (s:String) : ActionItem = new ActionItem (s)
   
-  val injectedActions = new TripleIdx[String,ActionItem,AssetCmdInjector]
-  val injectedFun = new TripleIdx[String,ActionItem,fun]
+    val injectedActions = new TripleIdx[String,String, (ActionItem,AssetCmdInjector)]
+    val injectedFun = new TripleIdx[String,ActionItem,fun]
 
     
-  /** classic Java type injection: create subclass with definition */
-  def inject (injected:AssetCmdInjector) : Unit = {
-    for (meta <- injected.entityTypes; action <- injected.actions) 
-      injectedActions.put(meta, action, injected)
-  }
+    /** classic Java type injection: create subclass with definition */
+    def inject (injected:AssetCmdInjector) : Unit = {
+        for (meta <- injected.entityTypes; action <- injected.actions)
+        injectedActions.put(meta, action.name, (action, injected))
+    }
   
-  /** new type inject */
-  def inject (meta:String, action:ActionItem, injected:fun) : Unit = {
-      injectedFun.put(meta, action, injected)
-  }
+    /** new type inject */
+    def inject (meta:String, action:ActionItem, injected:fun) : Unit = {
+        injectedFun.put(meta, action, injected)
+    }
 
-  /** */
-  def injection (t:String, cmd:String) : Option[fun] = {
-    //injectedActions.get2 (t,new ActionItem(cmd)) match {
-    injectedActions.get2 (t, new ActionItem(FuckYou.ORGANIZE.name)) match {
-      case Some(x) => Some(x.doAction _)
-      case None => injectedFun.get2(t,cmd)
-      }
-    //None
-  }
+    /** */
+    def injection (t:String, cmd:String) : Option[fun] = {
+        //injectedActions.get2 (t,new ActionItem(cmd)) match {
+        injectedActions.get2 (t, cmd) match {
+            case Some(x) => Some(x._2.doAction _)
+            case None => injectedFun.get2(t,cmd)
+        }
+        //None
+    }
   
-  def injections (t:String) : Array[ActionItem] = {
-    //var ret : Array[String] = Array()
-    var ret : List[ActionItem] = List()
+    def injections (t:String) : Array[ActionItem] = {
+        //var ret : Array[String] = Array()
+        var ret : List[ActionItem] = List()
     
-    ret = ret ++ injectedActions.get1(t)
-    ret = ret ++ injectedFun.get1(t)
+        val aa = injectedActions.get1v(t)
+        
+        for (x <- injectedActions.get1v(t))
+          ret = x._1 :: ret
+
+        ret = ret ++ injectedFun.get1k(t)
     
-    ret.reverse.toArray
-  }
+        ret.reverse.toArray
+    }
 }
--- src/com/razie/pub/data/TripleIdx.scala
+++ src/com/razie/pub/data/TripleIdx.scala
@@ -4,43 +4,56 @@ import com.razie.pub.base._
 import com.razie.pub.assets._
 import scala.collection._
 
-/** got only abstract methods so it turns into a java interface... */
+/** a simple triple index: each A has a set of (B,C*) */
 class TripleIdx[A, B, C] {
   
-  val idx = mutable.Map[A,mutable.Map[B,C]] ()
+    val idx = mutable.Map[A,mutable.Map[B,C]] ()
 
     def initMap (meta:A, ink:mutable.Map[A,mutable.Map[B, C]]):mutable.Map[B,C] = {
-      val m = ink.get(meta)
+        val m = ink.get(meta)
 
-      m match {
-        case None => {
-          val m:mutable.Map[B,C] = mutable.Map[B,C] ()
-          ink.put (meta, m)
-          m
+        m match {
+            case None => {
+                    val m:mutable.Map[B,C] = mutable.Map[B,C] ()
+                    ink.put (meta, m)
+                    m
+                }
+            case Some(x) => x
         }
-        case Some(x) => x
-      }
     }
 
-  /** new type inject */
-  def put (a:A, b:B, c:C) : Unit = {
-      initMap(a, idx).put (b, c)
-  }
+    /** new type inject */
+    def put (a:A, b:B, c:C) : Unit = {
+        initMap(a, idx).put (b, c)
+    }
 
-  /** */
-  def get2 (a:A, b:B) : Option[C] = {
-	if (idx.contains(a))
-      idx.get(a).get.get(b)
-    else 
-      None
-  }
-  
-  def get1 (a:A) : List[B] = {
-//    var ret : List[B] = List()
+    /** get the C for (a,b)*/
+    def get2 (a:A, b:B) : Option[C] = {
+        if (idx.contains(a))
+          idx.get(a).get.get(b)
+        else
+          None
+    }
+ 
+    /** get the list of B's for a*/
+    def get1k (a:A) : List[B] = {
+        if (idx.contains(a))
+          Nil ++ idx.get(a).get.keySet
+        else
+          Nil
+    }
     
-	if (idx.contains(a)) 
-	  Nil ++ idx.get(a).get.keySet
-    else
-      Nil   
-  }
+    /** get the list of C's for a*/
+    def get1v (a:A) : List[C] = {
+        if (idx.contains(a)) {
+          var ret:List[C] = List[C]()
+          
+          for (x<-idx.get(a).get.values)
+            ret = x :: ret
+          // TODO reverse may be useless since map doesn't preserve order?
+          ret.reverse
+          }
+        else
+          Nil
+    }
 }
