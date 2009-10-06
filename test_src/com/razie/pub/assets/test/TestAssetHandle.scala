306f9af25db2c3215a575260a556db3734ac6153
razie <razie@razie>
2009-10-06 12:42:34 +0000

    checkpoint

---
+++ test_src/com/razie/pub/assets/test/TestAssetHandle.scala
@@ -0,0 +1,33 @@
+/**
+ * Razvan's public code. Copyright 2008 based on Apache license (share alike) see LICENSE.txt for
+ * details.
+ */
+package com.razie.pub.assets.test;
+
+import org.scalatest.junit._
+import com.razie.pub.base.data._
+import org.scalatest.SuperSuite
+
+/**
+ * testing the assets
+ * 
+ * @author razvanc99
+ */
+class TestAssetHandle extends JUnit3Suite {
+
+   /**
+    */
+   def testSoaEchoHandle() = {
+       // send echo command
+//       val handle = new AssetHandle(TestLocalSoaAssets.PLAYERKEY);
+//       val result = handle.invoke("play", "movie", TestLocalSoaAssets.MOVIEKEY.toString());
+//
+//       assertTrue(result.contains(TestLocalSoaAssets.MOVIEKEY.getId()));
+   }
+
+ def test11a = expect (List("root")) {
+//  new XP("/root").xpl(new StringXqSolver, "/root")
+  }
+
+
+}
