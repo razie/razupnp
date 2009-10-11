a824ebe9f35d97ca1168068c39cfaf7c5e24bef7
razie <razie@razie>
2009-10-11 15:06:40 +0000

    fixed some junits

--- src/scala/razie/Threads.scala
+++ src/scala/razie/Threads.scala
@@ -3,12 +3,28 @@ package scala.razie
 
 object Threads {
    
-   def run (f: =>Unit) {
-      new java.lang.Thread(new java.lang.Runnable() {
+   def run (f: =>Unit) : java.lang.Thread = {
+      val thread = new java.lang.Thread(new java.lang.Runnable() {
          override def run() = {
             f
          }
-      }).start();
-
+      })
+      thread.start()
+      thread
+   }
+   
+   def foreach[A] (as:Iterable[A]) (f:A =>Unit) : Iterable[java.lang.Thread] = {
+	   val threads = for (a <- as) yield
+	      new java.lang.Thread(new java.lang.Runnable() {
+	         override def run() = {
+	            f(a)
+	         }
+	      })
+	   threads.foreach (_.start())
+	   threads
+	   }
+   
+   def join (threads:Iterable[java.lang.Thread]) = {
+      threads.foreach (_.join)
    }
 }
