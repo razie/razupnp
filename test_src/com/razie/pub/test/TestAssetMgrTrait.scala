6e8b1473a3a6de3b14af9756a4b5ad5c884de042
razie <razie@razie>
2009-12-03 18:48:36 +0000

    rewrite in scala - fixing svn issues

--- test_src/com/razie/pub/test/TestAssetMgrTrait.scala
+++ test_src/com/razie/pub/test/TestAssetMgrTrait.scala
@@ -3,13 +3,14 @@ package com.razie.pub.test
 import com.razie.pub.assets._
 import com.razie.pub.base._
 import org.scalatest.junit._
+import razie.assets._
 
 // just to instantiate a trait
-class SampleAssetMgrTrait extends AssetMgrTrait {
+class SampleAssetMgrTrait extends AssetMgrInjector {
 }
 
 // injected sample - has 1 type and 2 actions
-class SampleAJ extends AssetCmdInjector {
+class SampleAJ extends AssetCmdInjection {
    val entityTypes = Array("Movie")
    val actions = Array(new ActionItem("organize"), new ActionItem("moveto"))
    
@@ -19,7 +20,6 @@ class SampleAJ extends AssetCmdInjector {
   }
 }
 
-/** TODO proper junit */
 class TestAssetMgrTrait extends JUnit3Suite {
   
   type fun = (AssetKey, Referenceable, String, ScriptContext) => AnyRef
@@ -31,12 +31,12 @@ class TestAssetMgrTrait extends JUnit3Suite {
  
    am.inject(new SampleAJ)
    
-   //am.inject("Movie","organize",111)
+//   am.inject("Movie","organize",111)
    //am.inject("Movie","moveto",println ("moveto" + _ + _ + _ )))
 
   val aa = am.injections("Movie")
   println ("A: " + (aa mkString ","))
-   //Assert.assertTrue(idx.get1k("Movie")==List("organize", "moveto"))
+//   Assert.assertTrue(idx.get1k("Movie")==List("organize", "moveto"))
    //Assert.assertTrue(idx.get2("Movie","organize") == Some(111))
   }
 }
